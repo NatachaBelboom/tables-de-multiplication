6808295ffc0655070b007fb3e3fa106f973fb3f0
NatachaBelboom <natacha.belboom@student.hepl.be>
2021-02-15 14:35:38 +0100

    changed index

--- index.php
+++ index.php
@@ -1,37 +1,9 @@
-<?php
-define('SITE_TITLE', 'Les tables de multiplication');
-
-$tableCount = 0;
-$valueCount = 0;
-
-$errors = [];
-
-if (isset($_GET['nbvaleurs'], $_GET['nbtables'])) {
-    if (ctype_digit($_GET['nbvaleurs'])) {
-        $valueCount = (int) $_GET['nbvaleurs'];
-        if ($valueCount <= 1) {
-            $errors ['valueErrors'] = 'Je ne peux calculer les tables que s’il y a plus d’une valeur dans la table';
-        }
-    } else {
-        $errors ['valueErrors'] = 'Vous n’avez pas entré un entier positif pour déterminer le nombre de valeurs à calculer';
-    }
-    if (ctype_digit($_GET['nbtables'])) {
-        $tableCount = (int) $_GET['nbtables'];
-    } else {
-        $errors ['tableErrors'] = 'Vous n’avez pas entré un entier positif pour déterminer le nombre de tables à calculer';
-    }
-}
-
-
-?>
-
-<!-- Début du template d’affichage -->
 
 <!DOCTYPE html>
 <html lang="fr-be">
 <head>
     <meta charset="utf-8">
-    <title><?= SITE_TITLE ?></title>
+    <title>Les tables de multiplication</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -42,47 +14,74 @@ if (isset($_GET['nbvaleurs'], $_GET['nbtables'])) {
     <section>
         <h2>Indiquez quelles tables vous souhaitez</h2>
         <form action="index.php" method="get">
-            <div class="form-group<?= isset($errors['tableErrors']) ? ' has-error' : '' ?>">
+            <div class="form-group">
                 <label class="control-label" for="nbtables">Nombre de tables : </label>
                 <input class="form-control" id="nbtables" type="text" name="nbtables"
-                       value="<?= $tableCount ?>">
-                <?php if (isset($errors['tableErrors'])): ?>
-                    <span class="help-block"><?= $errors['tableErrors'] ?></span>
-                <?php endif ?>
+                       value="4">
             </div>
-            <div class="form-group<?= isset($errors['valueErrors']) ? ' has-error' : '' ?>">
+            <div class="form-group">
                 <label class="control-label" for="nbvaleurs">Nombre de valeurs : </label>
                 <input class="form-control" id="nbvaleurs" type="text" name="nbvaleurs"
-                       value="<?= $valueCount ?>">
-                <?php if (isset($errors['valueErrors'])): ?>
-                    <span class="help-block"><?= $errors['valueErrors'] ?></span>
-                <?php endif ?>
+                       value="6">
             </div>
             <input type="submit">
         </form>
     </section>
-    <?php if ($tableCount > 0 && $valueCount > 1): ?>
-        <section>
-            <h2>Voici vos tables</h2>
-            <table class="table table-striped table-bordered">
-                <caption>Les <?= $valueCount ?> premières valeurs des <?= $tableCount ?> premières tables</caption>
-                <tr>
-                    <th class="vide">&nbsp;</th>
-                    <?php for ($cell = 1; $cell <= $tableCount; $cell++): ?>
-                        <th scope="col"><?= $cell ?></th>
-                    <?php endfor ?>
-                </tr>
-                <?php for ($row = 1; $row <= $valueCount; $row++): ?>
-                    <tr>
-                        <th scope="row"><?= $row ?></th>
-                        <?php for ($cell = 1; $cell <= $tableCount; $cell++): ?>
-                            <td><?= $row ?> * <?= $cell ?> = <?= $row * $cell ?></td>
-                        <?php endfor ?>
-                    </tr>
-                <?php endfor ?>
-            </table>
-        </section>
-    <?php endif ?>
+    <section>
+        <h2>Voici vos tables</h2>
+        <table class="table table-striped table-bordered">
+            <caption>Les 6 premières valeurs des 4 premières tables</caption>
+            <tr>
+                <th class="vide">&nbsp;</th>
+                <th scope="col">1</th>
+                <th scope="col">2</th>
+                <th scope="col">3</th>
+                <th scope="col">4</th>
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td>1 * 1 = 1</td>
+                <td>1 * 2 = 2</td>
+                <td>1 * 3 = 3</td>
+                <td>1 * 4 = 4</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>2 * 1 = 2</td>
+                <td>2 * 2 = 4</td>
+                <td>2 * 3 = 6</td>
+                <td>2 * 4 = 8</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>3 * 1 = 3</td>
+                <td>3 * 2 = 6</td>
+                <td>3 * 3 = 9</td>
+                <td>3 * 4 = 12</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>4 * 1 = 4</td>
+                <td>4 * 2 = 8</td>
+                <td>4 * 3 = 12</td>
+                <td>4 * 4 = 16</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>5 * 1 = 5</td>
+                <td>5 * 2 = 10</td>
+                <td>5 * 3 = 15</td>
+                <td>5 * 4 = 20</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>6 * 1 = 6</td>
+                <td>6 * 2 = 12</td>
+                <td>6 * 3 = 18</td>
+                <td>6 * 4 = 24</td>
+            </tr>
+        </table>
+    </section>
 </main>
 </body>
 </html>
